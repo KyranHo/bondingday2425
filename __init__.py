8cd8c926f166ae3eb897fa0d1400e5d74f8127c0
Kyran <pecksengho@gmail.com>
2024-12-09 15:39:41 +0800

    v1.4.2

--- __init__.py
+++ __init__.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/first')
+@app.route('/')
 def first():
     return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=81, debug=True)
+  app.run()
