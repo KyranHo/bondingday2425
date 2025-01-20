a285aea8e39d1f2fa646322d677c984489a642c5
Kyran <pecksengho@gmail.com>
2025-01-20 12:17:09 +0800

    v2.0
    Bootstrap Implementation

--- __init__.py
+++ __init__.py
@@ -2,25 +2,49 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
+#Start
 @app.route('/')
 def first():
     return render_template('index.html')
 
-@app.route('/room')
-def mentoringclub():
-    return render_template('room.html')
+@app.route('/Clock')
+def clock():
+    return render_template('clock.html')
 
-@app.route('/lift')
+
+@app.route('/lift') 
 def lift():
     return render_template('lift.html')
 
 
-#####################################################
+@app.route('/room') 
+def mentoringclub():
+    return render_template('room.html')
 
-@app.route('/d3c0y')
+
+@app.route('/comp') 
+def comp():
+    return render_template('comp.html')
+
+
+@app.route('/pillar') 
+def pillar():
+    return render_template('pillar.html')
+
+#Final Main - Placed at xxx
+@app.route('/random') 
+def location():
+    return 'lol'
+
+#####################################################
+#Decoy Sites
+@app.route('/d3c0y') #Placed at corner of E.318
 def decoy1():
     return render_template('decoy1.html')
+
+@app.route('/dec0y') #Placed at
+def decoy2():
+    return "Decoy"
 
 #####################################################
 if __name__ == '__main__':
