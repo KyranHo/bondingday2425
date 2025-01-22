1e40487b298da69faf0e81f503f72ee625864a18
Kyran <pecksengho@gmail.com>
2025-01-22 21:57:52 +0800

    v2.2
    99% completion of pages, except comp.html and vending.html

--- __init__.py
+++ __init__.py
@@ -7,43 +7,74 @@ app = Flask(__name__)
 def first():
     return render_template('index.html')
 
+#Tier 1
 #At clock
-@app.route('/Clock')
+@app.route('/clock')
 def clock():
     return render_template('clock.html')
 
-
-@app.route('/lift') 
-def lift():
-    return render_template('lift.html')
-
-
-@app.route('/room') 
+#Tier 2
+#Outside CCA room
+@app.route('/mcroom') 
 def mentoringclub():
     return render_template('room.html')
 
+#Snack corner
+@app.route('/snack')
+def snack():
+    return render_template('snack.html')
 
-@app.route('/comp') 
-def comp():
-    return render_template('comp.html')
-
-
+#Tier 3
+#Pillar
 @app.route('/pillar') 
 def pillar():
     return render_template('pillar.html')
 
+#Pillar with bins
+@app.route('/pillarbins')
+def pillarbins():
+    return render_template('pillarbins.html')
+
+#Lift
+@app.route('/lift') 
+def lift():
+    return render_template('lift.html')
+
+#Event venue
+@app.route('/event')
+def event():
+    return render_template('event.html')
+
+#Tier 4
+#Displayed on computer while I sleep
+@app.route('/comp') 
+def comp():
+    return render_template('comp.html')
+
+#Displayed near vending machine
+@app.route('/vend')
+def vend():
+    return render_template('vend.html')
+
 #Final Main - Placed at xxx
 @app.route('/random') 
 def location():
-    return 'lol'
+    return render_template('random.html')
+
+#####################################################
+#Wrong place
+#Stairs
+@app.route('/stairs')
+def stairs():
+    return render_template('stairs.html')
 
 #####################################################
 #Decoy Sites
-@app.route('/d3c0y') #Placed at corner of E.318
+@app.route('/d3c0y')
 def decoy1():
-    return render_template('decoy1.html')
+    return render_template('decoys/decoy1.html')
 
-@app.route('/dec0y') #Placed at
+@app.route('/dec0y')
 def decoy2():
     return "Decoy"
 
